5eef14080daef4c01bdc9faa46064aad4f6e5077
Joel-Romero <joelandres4ever@gmail.com>
2024-08-02 13:13:13 -0500

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import { Registro } from './src/screen/Registro';
+import { Inicio } from './src/screen/Inicio';
+
+
+export const DEBER2 = () => { 
+  return ( 
+  <Registro/>
+  //<Inicio/>
+  ) 
+} 
+ 
+export default DEBER2;
